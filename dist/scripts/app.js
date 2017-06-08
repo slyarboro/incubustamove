68512cd44982efd74440fd2d5039aa7b3d311d9b
Your Name <yourname@example.com>
2017-06-08 18:48:32 -0400

    Routing and States CP(3): Complete

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,29 @@
-angular.module( "miggidamac", [ ] );
+// ng providers injected into configuration block, granting application full access to the providers
+// $state creates new application state (object/string)
+// $location determines how linking paths will be stored
+(function() {
+    function config($stateProvider, $locationProvider) {
+      $locationProvider
+        .html5Mode({
+          //  user sees clean URLs sans hashbang
+          enabled: true,
+          requireBase: false
+      });
+
+      $stateProvider
+        .state('landing', {
+          url: '/',
+          templateUrl: '/templates/landing.html'
+        })
+
+        .state('album', {
+          url: '/album',
+          templateUrl: '/templates/album.html'
+        });
+    }
+
+    angular
+      .module('miggidamac', ['ui.router'])
+      .config(config);
+
+})();
